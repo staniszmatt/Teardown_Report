5a150fac34046d9e6a4ca39a179a3ea9c0fd63a5
staniszmatt <matthewpstaniszewski@gmail.com>
2022-05-16 09:58:58 -0600

    Setup two test pages to verify routes Setup Links to two test page components Setup routes to the test pages Setup styling to confirm different test page links work to bring up each individual page.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,18 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route, Link } from 'react-router-dom';
 import icon from '../../assets/icons/logo.png';
 import './App.css';
+import TP1 from './components/TestPageOne';
+import TP2 from './components/TestPageTwo';
 
-const Hello = () => {
+const TearDownApp = () => {
   return (
     <div>
-      <div className="Hello">
+      <div className="container">
         <img width="200px" alt="icon" src={icon} />
+      </div>
+      <div>
+        <Link to="TestPageOne">Test Page One</Link>
+        <Link to="TestPageTwo">Test Page Two</Link>
       </div>
     </div>
   );
@@ -14,10 +20,12 @@ const Hello = () => {
 
 export default function App() {
   return (
-    <Router>
+    <MemoryRouter>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<TearDownApp />} />
+        <Route path="TestPageOne" element={<TP1 />} />
+        <Route path="TestPageTwo" element={<TP2 />} />
       </Routes>
-    </Router>
+    </MemoryRouter>
   );
 }
---
+++ src/renderer/components/TestPageOne.tsx
@@ -0,0 +1,10 @@
+import './TestPageStyle.css';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function TestPageOne() {
+  return (
+    <div className="testPageContainerOne">
+      <h1>Test Page One</h1>
+    </div>
+  );
+}
